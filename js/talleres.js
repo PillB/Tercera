04923364b21cdff9cfa516fe97a98a15ebd904f5
PillB <11358989+PillB@users.noreply.github.com>
2023-11-20 14:59:15 -0500

    Added map fallback logic
    When no valid API key found

--- js/talleres.js
+++ js/talleres.js
@@ -48,17 +48,50 @@ document.addEventListener('DOMContentLoaded', function() {
     function populateTopicFilter(data) {
         const topicSet = new Set();
         data.forEach(workshop => {
-            topicSet.add(workshop.topic);
+            // Convert topic to lower case before adding to set
+            topicSet.add(workshop.topic.toLowerCase());
         });
-
+    
         const topicSelect = document.getElementById('topic');
         topicSet.forEach(topic => {
             const option = document.createElement('option');
             option.value = topic;
-            option.textContent = topic.charAt(0).toUpperCase() + topic.slice(1); // Capitalize first letter
+            // Capitalize first letter for display
+            option.textContent = topic.charAt(0).toUpperCase() + topic.slice(1);
             topicSelect.appendChild(option);
         });
     }
+    
+    function initMap() {
+        try {
+            var mapOptions = {
+                zoom: 8,
+                center: { lat: -34.397, lng: 150.644 }, // Example coordinates
+            };
+            var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+            // Add markers for each workshop location
+            // Example: new google.maps.Marker({ position: { lat: -34.397, lng: 150.644 }, map: map });
+            // Add more markers as needed
+        } catch (error) {
+            console.error('Google Maps API error:', error);
+            displayStaticMapFallback();
+        }
+    }
+    
+    function displayStaticMapFallback() {
+        const mapContainer = document.getElementById('map');
+        mapContainer.innerHTML = `<iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.3821975497084!2d-77.04279388518705!3d-12.046374045576708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b8405c6f8c9f%3A0x5d4c3e4c9f1b8e0!2sLima!5e0!3m2!1sen!2spe!4v1645631258431!5m2!1sen!2spe"
+            width="600"
+            height="450"
+            style="border:0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>`;
+    }
+        
 });
 
 
