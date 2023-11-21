aaa38736e3b463586fd3558236c2655179746c5c
PillB <11358989+PillB@users.noreply.github.com>
2023-11-20 19:38:00 -0500

    Added post logic

--- js/publica.js
+++ js/publica.js
@@ -1,13 +1,49 @@
-tinymce.init({
-    selector: '#description'
-});
+// Initialize TinyMCE Editor
+tinymce.init({ selector: '#description' });
 
+// Initialize Dropzone for image upload
 Dropzone.options.imageUpload = {
-    url: '/upload-image', // Specify your server script to handle the upload
-    maxFiles: 1, // Adjust based on your need
+    url: '/upload-image', // Server script for handling uploads
+    maxFiles: 1,
     acceptedFiles: 'image/*',
     dictDefaultMessage: 'Drop your image here or click to upload (Only images)',
-    // Additional options can be added here
 };
 
+// Initialize Tagify for tagging
 new Tagify(document.querySelector('#tags'));
+
+// Form submission with AJAX and DOMPurify
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Get form data
+    const title = DOMPurify.sanitize(document.getElementById('title').value);
+    const description = tinymce.get('description').getContent();
+    const category = DOMPurify.sanitize(document.getElementById('category').value);
+    const tags = DOMPurify.sanitize(document.getElementById('tags').value);
+    const image = document.querySelector('#image-upload').dropzone.files[0]; // Get the uploaded file
+
+    // Create FormData object for AJAX request
+    let formData = new FormData();
+    formData.append('title', title);
+    formData.append('description', description);
+    formData.append('category', category);
+    formData.append('tags', tags);
+    if (image) formData.append('image', image);
+
+    // AJAX request to server
+    fetch('/submit-project', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle response
+        console.log('Success:', data);
+        // Redirect or show success message
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        // Handle errors here
+    });
+});
